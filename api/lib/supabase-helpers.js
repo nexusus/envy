9f837731ad380482891b57a2ed6c3555fa8b3c85
nexusus <62360473+nexusus@users.noreply.github.com>
2025-08-16 22:55:45 +0300

    check stuff2

--- api/lib/supabase-helpers.js
+++ api/lib/supabase-helpers.js
@@ -4,7 +4,7 @@ const { SUPABASE_URL, SUPABASE_ANON_KEY } = require('./config');
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function getWhitelistRank(robloxUsername) {
-  print(robloxUsername)
+  console.log(robloxUsername)
   const { data, error } = await supabase
     .from('whitelists')
     .select('rank')
