852117509a4eae9ba573c663e75cec660223cf04
nexusus <62360473+nexusus@users.noreply.github.com>
2025-08-16 21:39:43 +0300

    check stuff2

--- api/lib/supabase-helpers.js
+++ api/lib/supabase-helpers.js
@@ -7,7 +7,7 @@ async function getWhitelistRank(robloxUsername) {
     const { data, error } = await supabase
         .from('whitelists')
         .select('rank')
-        .contains('roblox_username', [robloxUsername])
+        .eq('roblox_username', robloxUsername)
         .single();
 
     if (error) {
